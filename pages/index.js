1534ce893419085ebee821e47b39ff0c98e3c750
Jefferson <137832123+jeffersonlouis@users.noreply.github.com>
2023-07-01 23:56:48 +0000

    Deploying my first message to Sabrina

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home()
 {
-    return <h1>Test 1</h1>
+    return <><h1>Oii Amor ❤️</h1><h3> Olha no que já estou conseguindo fazer, colocando um site na internet para o mundo inteiro poder acessar, o nome disso é Deploy, muito legal né?! 
+        <br></br>É só um pequenino paso, mas um dia vou longe..
+        😄😄😄😄😄
+        </h3></>
 }
 
 
